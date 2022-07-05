619b88e8a09f97e182ea939c86f81d90cec79feb
- <>
2022-07-05 08:33:04 +0200

    edit

---
+++ Chambers/Langs-Long/arabe/Arabic-learning/Mehmet-Bahattin-Arabe/latinized/render.bash
@@ -0,0 +1,56 @@
+
+
+cp Muhammad_Bagauddin-latinized.html current.txt
+
+
+
+
+ sed -i.bak 's/َا/ā/g' current.txt
+ sed -i.bak 's/ِي/ī/g' current.txt
+ sed -i.bak 's/ُو/ū/g' current.txt
+ sed -i.bak 's/َي/ay/g' current.txt
+ sed -i.bak 's/َو/aw/g' current.txt
+ sed -i.bak 's/ء/ʔ/g' current.txt
+ sed -i.bak 's/ا/ā/g' current.txt
+ sed -i.bak 's/ب/b/g' current.txt
+ sed -i.bak 's/ت/t/g' current.txt
+ sed -i.bak 's/ث/θ/g' current.txt
+ sed -i.bak 's/ج/j/g' current.txt
+ sed -i.bak 's/ح/ḥ/g' current.txt
+ sed -i.bak 's/خ/x/g' current.txt
+ sed -i.bak 's/د/d/g' current.txt
+ sed -i.bak 's/ذ/ð/g' current.txt
+ sed -i.bak 's/ر/r/g' current.txt
+ sed -i.bak 's/ز/z/g' current.txt
+ sed -i.bak 's/س/s/g' current.txt
+ sed -i.bak 's/ش/š/g' current.txt
+ sed -i.bak 's/ص/ṣ/g' current.txt
+ sed -i.bak 's/ض/ḍ/g' current.txt
+ sed -i.bak 's/ط/ṭ/g' current.txt
+ sed -i.bak 's/ظ/ẓ/g' current.txt
+ sed -i.bak 's/ع/ʕ/g' current.txt
+ sed -i.bak 's/غ/ɣ/g' current.txt
+ sed -i.bak 's/ف/f/g' current.txt
+ sed -i.bak 's/ق/q/g' current.txt
+ sed -i.bak 's/ك/k/g' current.txt
+ sed -i.bak 's/ل/l/g' current.txt
+ sed -i.bak 's/م/m/g' current.txt
+ sed -i.bak 's/ن/n/g' current.txt
+ sed -i.bak 's/ه/h/g' current.txt
+ sed -i.bak 's/و/w/g' current.txt
+ sed -i.bak 's/ي/y/g' current.txt
+ sed -i.bak 's/آ/ʔā/g' current.txt
+ sed -i.bak 's/ة/ᵗ/g' current.txt
+ sed -i.bak 's/ى/y/g' current.txt
+ sed -i.bak 's/َ/a/g' current.txt
+ sed -i.bak 's/ِ/i/g' current.txt
+ sed -i.bak 's/ُ/ᵘ/g' current.txt
+ sed -i.bak 's/ً/ᵃⁿ/g' current.txt
+ sed -i.bak 's/ٍ/ⁱⁿ/g' current.txt
+ sed -i.bak 's/ٌ/ᵘⁿ/g' current.txt
+ sed -i.bak 's/ْ//g' current.txt
+
+
+
+cp current.txt result.html
+
